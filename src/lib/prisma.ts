356b9942d19d029a93996e5afd3df297dfab7c6d
Rasif Al Saif <rasifalsaif@gmail.com>
2026-02-26 16:55:11 +0600

    issues trying to solve

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from "pg";
-import { PrismaClient } from '../../generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
